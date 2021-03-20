646702ab4ffc90ffbcf71f07ad27fd0d6f5d71d1
pythonist <44141931+pythonist@users.noreply.github.com>
2021-03-20 16:19:24 +0530

    Update pnumapp.py

--- pnumapp.py
+++ pnumapp.py
@@ -82,7 +82,7 @@ def upload():
 
     #this section is used by gunicorn to serve the app on Heroku
 if __name__ == '__main__':
-        app.run()
+    app.run(debug=True)
     #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
     # Serve the app with gevent 
     #http_server = WSGIServer(('', 5000), app)
